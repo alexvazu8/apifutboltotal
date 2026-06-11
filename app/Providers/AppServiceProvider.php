bfb8e55f46667ca3fdd645483254405854a8105f
alexvazu8 <alexvazu@gmail.com>
2026-06-10 20:48:34 -0400

    tercer commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-            if (app()->environment('production')) {
-                    URL::forceScheme('https');
-            }
             Scramble::configure()
             ->withDocumentTransformers(function (OpenApi $openApi) {
                 $openApi->secure(
